2a0266c3cb5dfe7842ebc71b54962de3efbd87a2
Monique <monique.townson@gmail.com>
2021-02-23 09:09:09 -0800

    Front End deployment

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -42,9 +42,9 @@ const cssFilename = 'static/css/[name].[contenthash:8].css'
 // However, our output is structured with css, js and media folders.
 // To have this structure working with relative paths, we have to use custom options.
 const extractTextPluginOptions = shouldUseRelativeAssetPaths
-  ? // Making sure that the publicPath goes back to to build folder.
-    { publicPath: Array(cssFilename.split('/').length).join('../') }
-  : {}
+  // Making sure that the publicPath goes back to to build folder.
+  { publicPath: Array(cssFilename.split('/').length).join('../') }
+ :{}
 
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
@@ -71,7 +71,7 @@ module.exports = {
     devtoolModuleFilenameTemplate: info =>
       path
         .relative(paths.appSrc, info.absoluteResourcePath)
-        .replace(/\\/g, '/'),
+        .replace(/\\/g, '/')
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
