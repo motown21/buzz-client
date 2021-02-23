351754f7021d447b1719d7b4ab1b8b76a8d9595f
Monique <monique.townson@gmail.com>
2021-02-23 05:57:47 -0800

    Deployment Commmit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,3 @@
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import CardDeck from 'react-bootstrap/CardDeck'
+// import Button from 'react-bootstrap/Button'
+// import Card from 'react-bootstrap/Card'
+// import CardDeck from 'react-bootstrap/CardDeck'
